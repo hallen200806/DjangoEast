a38500fa9517d77cb5f4fb5248ef74526fc8a336
reborn <reborn0502@163.com>
2019-03-07 15:49:39 +0800

    修改STATIC_ROOT地址

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -155,11 +155,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = 'static'
-STATIC_ROOT = os.path.join(BASE_DIR,'/static/')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 
-# SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
-# STATICFILES_DIRS = (
-#   os.path.join(SITE_ROOT, 'static/'),
-# )
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static/"),
+# ]
