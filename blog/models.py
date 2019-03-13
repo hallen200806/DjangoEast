dcacf7b88258666a086e10ea5ded8ba7f5d55e04
reborn <reborn0502@163.com>
2019-03-13 16:33:34 +0800

    为book的的cover字段添加blank=true

--- blog/models.py
+++ blog/models.py
@@ -105,7 +105,7 @@ class Book(models.Model):
 	name = models.CharField(max_length=100,verbose_name="书名")
 	author = models.CharField(max_length=100, verbose_name="作者")
 	category = models.ForeignKey(BookCategory,on_delete=models.CASCADE,verbose_name="书籍分类")
-	cover = models.ImageField(upload_to='books',verbose_name="封面图")
+	cover = models.ImageField(upload_to='books',verbose_name="封面图",blank=True)
 	score = models.DecimalField(max_digits=2,decimal_places=1,verbose_name="豆瓣评分")
 	title = models.CharField(max_length=100, verbose_name="标题",default=" ")
 	detail = MDTextField(verbose_name="读书笔记", null=True)
