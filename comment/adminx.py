b55cc3282eed83c5394aa8ecdae117c040b21094
reborn <reborn0502@163.com>
2019-04-01 19:49:56 +0800

    后台注册comment

--- comment/adminx.py
+++ comment/adminx.py
@@ -1,9 +1,7 @@
-
 import xadmin
 from .models import *
 
 class CommentAdmin(object):
     list_display = ['content_object','text','comment_time','user']
-
 
 xadmin.site.register(Comment,CommentAdmin)
