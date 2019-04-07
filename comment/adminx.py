35d9c60eff273693852a3ffac579a4b48a5419f9
reborn <reborn0502@163.com>
2019-04-07 16:59:05 +0800

    修复电影详情无标题BUG

--- comment/adminx.py
+++ comment/adminx.py
@@ -3,7 +3,7 @@ from .models import *
 
 class CommentAdmin(object):
     list_display = ['id','object_id','text','comment_time','user']
-    search_fields = ['content_object']
+    search_fields = ['content_type']
     model_icon = 'fa fa-comment'
 
 xadmin.site.register(Comment,CommentAdmin)
