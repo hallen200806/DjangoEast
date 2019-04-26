7a3678e0b266f4d93f5a4259d8e3e07811fca8ef
reborn <reborn0502@163.com>
2019-04-26 10:34:07 +0800

    wechat test

--- robot.py
+++ robot.py
@@ -4,6 +4,6 @@ from werobot import WeRoBot
 robot = WeRoBot(token='weixin')
 @robot.handler
 def hello(message):
-    msg = message
-    if message == 'python':
+    msg = message.content.strip().lower().encode('utf8')
+    if msg == 'python':
         return '想学python啊'
